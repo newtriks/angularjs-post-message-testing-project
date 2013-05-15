a570f81c5f6caea38b4d4607fc2b14d6fdfd9f63
Simon Bailey <simon@newtriks.com>
2013-05-15 12:57:06 +0100

    Added service tests
    Signed-off-by: Simon Bailey <simon@newtriks.com>

--- test/spec/services/postMessageService.js
+++ test/spec/services/postMessageService.js
@@ -1,18 +1,49 @@
 'use strict';
 
-describe('Service: postMessageService', function () {
+describe('Service: postMessageService', function() {
 
   // load the service's module
   beforeEach(module('iframeCommunicationApp'));
 
   // instantiate service
-  var postMessageService;
-  beforeEach(inject(function (_postMessageService_) {
+  var postMessageService, messages, rootScope;
+
+  beforeEach(inject(function($rootScope, _postMessageService_) {
     postMessageService = _postMessageService_;
+    rootScope = $rootScope;
+    messages = ["foo", "bar"];
   }));
 
-  it('should do something', function () {
-    expect(!!postMessageService).toBe(true);
+  it('should have a service', function() {
+    expect(postMessageService).not.toEqual(null);
+  });
+
+  it('should have no messages', function() {
+    expect(postMessageService.messages[0]).toBeUndefined();
+  });
+
+  it('should store a message in the messages array', function() {
+    var m = postMessageService.messages(messages[0]);
+    expect(m).not.toBeUndefined();
+  });
+
+  it('should store and retrieve the correct message', function() {
+    var m = postMessageService.messages(messages[0]);
+    expect(m[0]).toEqual(messages[0]);
+  });
+
+  it('should broadcast an outgoing message', function() {
+    var outgoingMessageListener = jasmine.createSpy('listener');
+    rootScope.$on('outgoingMessage', outgoingMessageListener);
+    postMessageService.outgoing(messages[0]);
+    expect(outgoingMessageListener).toHaveBeenCalled();
+  });
+
+  it('should broadcast the correct outgoing message', function() {
+    var outgoingMessageListener = jasmine.createSpy('listener');
+    rootScope.$on('outgoingMessage', outgoingMessageListener);
+    postMessageService.outgoing(messages[0]);
+    expect(outgoingMessageListener.mostRecentCall.args[1]).toEqual(messages[0]);
   });
 
 });
